16227c925b9413ac4b5aceff8027da4c7654e744
wangsongiam <wangsongiam@gmail.com>
2017-01-03 09:52:53 -0500

    preset 2017

--- spread.js
+++ spread.js
@@ -1,7 +1,11 @@
-const person = { name: 'Song'}        
-const newPerson = Object.assign(person, { nickname: 'Wang'})
-
-console.log(newPerson)
+// const person = {name: 'Song'}
+// const newPerson = Object.assign(person, {nickname: 'Wang'})
 
 
+// const place = {
+//   name: 'Beijing',
+//   location: 'China'
+// }
+
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
 
